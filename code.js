be054c6f84b7b5409b1eb24856bf16b59f3a756b
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-22 16:40:36 -0600

    code.js

--- code.js
+++ code.js
@@ -1,3 +1,38 @@
 function quicksort(array) {
+    let stack = [];
+    stack.push(0);
+    stack.push(array.length - 1);
+    while (stack.length > 0) {
+        let high = stack.pop();
+        let low = stack.pop();
+        let pivotIndex = makePivot(arr, low, high);
+        if (pivotIndex - 1 > low) {
+            stack.push(low);
+            stack.push(pivotIndex - 1);
+        }
+        if (pivotIndex + 1 < high) {
+            stack.push(pivotIndex + 1);
+            stack.push(high);
+        }
+    }
     return array;
+}
+
+function makePivot(array, low, high) {
+    let pivot = array[high];
+    let i = low - 1;
+    for (let j = low; j < high; j++) {
+        if (array[j] < pivot) {
+            swapElements(arr, i, j);
+            i++
+        }
+    }
+    swapElements(array, i + 1, high);
+    return i + 1;
+}
+
+function swapElements(array, i, j) {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
