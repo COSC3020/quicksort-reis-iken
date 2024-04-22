00b9c2c3fc108265dba3f414050c7391115f406f
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-22 17:26:48 -0600

    code.js

--- code.js
+++ code.js
@@ -3,17 +3,17 @@ function quicksort(array, low, high) {
     stack.push({ low: low, high: high });
     while (stack.length > 0) {
         let { low, high } = stack.pop();
-        let pivotIndex = makePivot(array, low, high);
-        if (pivotIndex - 1 > low) {
-            stack.push({ low: low, high: pivotIndex - 1 });
-        }
-        if (pivotIndex + 1 < high) {
-            stack.push({ low: pivotIndex + 1, high: high });
-        }
-        if (low >= high) {
-            continue;
+        if (low < high) {
+            let pivotIndex = makePivot(array, low, high);
+            if (pivotIndex - 1 > low) {
+                stack.push({ low: low, high: pivotIndex - 1 });
+            }
+            if (pivotIndex + 1 < high) {
+                stack.push({ low: pivotIndex + 1, high: high });
+            }
         }
     }
+    return array;
 }
 
 function makePivot(array, low, high) {
